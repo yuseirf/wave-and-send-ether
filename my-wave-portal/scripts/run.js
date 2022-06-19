32f806f7fdf939836348bebdd20fa5916d7203c8
yuseirf <yusei.tanaka2112325@icloud.com>
2022-06-19 19:58:06 +0900

    feat: create wave-and-send-ether ローカルサーバーを立ち上げて、メッセージと0.0001ETHをwave（👋）し、contract addressに送ることができるようにした ブロックチェーン上にデータも保存できるようにした

---
+++ my-wave-portal/scripts/run.js
@@ -0,0 +1,52 @@
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  /*
+    * 0.1ETHをコントラクトに提供してデプロイする
+    */
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
+  await waveContract.deployed();
+  console.log("Contract deployed to:", waveContract.address);
+
+  /*
+    * コントラクトの残高を取得し、結果を出力（0.1ETHであることを確認）
+    */
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  /*
+    * Waveし、トランザクションが完了するまで待機
+    */
+  let waveTxn = await waveContract.wave("A message!");
+  await waveTxn.wait();
+
+  /*
+    * Waveした後のコントラクトの残高を取得し、結果を出力（0.0001ETH引かれていることを確認）
+    */
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
